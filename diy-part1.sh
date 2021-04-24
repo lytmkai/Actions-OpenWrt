aeb425e5d7e0e7f81df1dd8928775a7f262c4e11
3wking <43069914+3wking@users.noreply.github.com>
2021-04-25 02:42:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,8 @@
 
 # fw876/helloworld
 git clone https://github.com/3wking/openwrt-packages.git package/luci-app-openclash
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Lienol/openwrt-package
 # sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
