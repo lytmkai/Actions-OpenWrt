f260ecdcb9a3e82dd4240c1998533a6dcf078368
3wking <43069914+3wking@users.noreply.github.com>
2021-04-25 16:17:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,4 +10,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
+# Modify default Theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
