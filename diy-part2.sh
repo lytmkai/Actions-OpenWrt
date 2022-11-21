0a3142ddb8218f443797a52b5c4241148c9da787
lytmkai <lytmkai@qq.com>
2022-11-21 21:51:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/192.168./10.0./g' package/base-files/files/bin/config_generate
+
+sed -i "/\$netm/a\                                set.network.\$1.gateway='10.0.0.1'"     package/base-files/files/bin/config_generate
+
+sed -i "s/network.globals.ula_prefix='auto'/network.globals.ula_prefix='fd00::1'/g"     package/base-files/files/bin/config_generate
+
+
+
+
+
+
+
+
+
 
 # Modify default Theme
 sed -i 's/luci-theme-bootstrap/luci-theme-mcat/g' feeds/luci/collections/luci/Makefile
