4423114462867673a9baa8e044030f81f71db4c0
lytmkai <lytmkai@qq.com>
2023-02-21 18:36:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,3 +12,6 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
+
+
+go env -w GO111MODULE=off
